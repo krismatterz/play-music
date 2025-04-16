3b9fc214fa05337876d8b716e9f95057206162bc
krismatterz <dev@krismatterz.com>
2025-04-17 00:57:57 +0200

    chore: remove .next from git cache

--- apps/play/.next/transform.js
+++
@@ -1,7 +0,0 @@
-const CHUNK_PUBLIC_PATH = "transform.js";
-const runtime = require("./build/chunks/[turbopack]_runtime.js");
-runtime.loadChunk("build/chunks/apps_play_postcss_config_js_transform_ts_ee834be7._.js");
-runtime.loadChunk("build/chunks/[root-of-the-server]__878e5bb7._.js");
-runtime.getOrInstantiateRuntimeModule("[turbopack-node]/globals.ts [postcss] (ecmascript)", CHUNK_PUBLIC_PATH);
-runtime.getOrInstantiateRuntimeModule("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[project]/apps/play/postcss.config.js/transform.ts { CONFIG => \\\"[project]/apps/play/postcss.config.js_.loader.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[project]/apps/play/postcss.config.js/transform.ts { CONFIG => \\\"[project]/apps/play/postcss.config.js_.loader.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
